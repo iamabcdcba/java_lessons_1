d15aca2012d88a5138be3e8b870f03de75bbddca
iamabcdcba <iamabcdcba@gmail.com>
2017-05-21 20:49:56 +0300

    add java program structure

---
+++ Example.java
@@ -0,0 +1,10 @@
+/*
+  Это простая Java-программа.
+  Назовите этот файл "Example.java"
+*/
+class Example {
+    // Программа начинается с вызова main().
+    public static void main(String args[]) {
+	System.out.println("Это простая Java-программа.");
+    }
+}
